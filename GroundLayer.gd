896d0bb9924528f5f43b19b3dbc34d5cf037f21e
Mo-BiuS <lodland95@gmail.com>
2025-06-19 18:02:13 +0200

    Working more or less as intended

--- GroundLayer.gd
+++ GroundLayer.gd
@@ -1,6 +1,7 @@
 extends TileMapLayer
 
 @onready var shaderMaterial = material as ShaderMaterial
+var time:float
 
 func generateBitcodeMap() -> Array[int]:
 	var size:Vector2i = get_used_rect().size
@@ -10,15 +11,19 @@ func generateBitcodeMap() -> Array[int]:
 		for y in range(32):
 			var bit:int = 0;
 			if get_cell_source_id(Vector2i(x,y)) != -1 :
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   + Vector2i.LEFT ) == -1 : bit+=1
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   + Vector2i.RIGHT) == -1 : bit+=2
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN + Vector2i.RIGHT) == -1 : bit+=4
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN + Vector2i.LEFT ) == -1 : bit+=8
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   + Vector2i.LEFT ) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.UP  ) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.LEFT ) == -1 : bit+=1
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   + Vector2i.RIGHT) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.UP  ) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.RIGHT) == -1 : bit+=2
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN + Vector2i.RIGHT) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.RIGHT) == -1 : bit+=4
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN + Vector2i.LEFT ) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN) == -1 || get_cell_source_id(Vector2i(x,y) + Vector2i.LEFT ) == -1  : bit+=8
 			rep.append(bit)
 	
 	return rep
 
 func _ready() -> void:
 	shaderMaterial.set_shader_parameter("bitcodeMap", generateBitcodeMap())
-	shaderMaterial.set_shader_parameter("offSet", get_used_rect().position)
+	shaderMaterial.set_shader_parameter("scale", scale.x)
 	shaderMaterial.set_shader_parameter("tileSize", tile_set.tile_size)
+
+func _process(delta: float) -> void:
+	time += delta/2
+	shaderMaterial.set_shader_parameter("tcos", sin(time)*.2)
