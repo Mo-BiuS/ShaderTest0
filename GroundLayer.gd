0d4ad7b6da61969509db8ff1483f0fa94a144716
Mo-BiuS <lodland95@gmail.com>
2025-06-19 15:36:54 +0200

    NOW the data is transfered and organized ><

--- GroundLayer.gd
+++ GroundLayer.gd
@@ -2,22 +2,23 @@ extends TileMapLayer
 
 @onready var shaderMaterial = material as ShaderMaterial
 
-func generateBitcodeMap() -> ImageTexture:
+func generateBitcodeMap() -> Array[int]:
 	var size:Vector2i = get_used_rect().size
-	var image:Image = Image.create(size.x,size.y,false,Image.FORMAT_R8)
+	var rep:Array[int]
 	
-	for x in size.x:
-		for y in size.y:
+	for x in range(32):
+		for y in range(32):
 			var bit:int = 0;
-			if get_cell_source_id(Vector2i(x,y)) == -1 : bit=16
-			else:
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   ) == -1 : bit+=1
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.RIGHT) == -1 : bit+=2
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN ) == -1 : bit+=4
-				if get_cell_source_id(Vector2i(x,y) + Vector2i.LEFT ) == -1 : bit+=8
-			image.set_pixel(x,y,Color(bit,0,0))
+			if get_cell_source_id(Vector2i(x,y)) != -1 :
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   + Vector2i.LEFT ) == -1 : bit+=1
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.UP   + Vector2i.RIGHT) == -1 : bit+=2
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN + Vector2i.RIGHT) == -1 : bit+=4
+				if get_cell_source_id(Vector2i(x,y) + Vector2i.DOWN + Vector2i.LEFT ) == -1 : bit+=8
+			rep.append(bit)
 	
-	return ImageTexture.create_from_image(image)
+	return rep
 
 func _ready() -> void:
-	shaderMaterial.set_shader_parameter("proximityMap", generateBitcodeMap())
+	shaderMaterial.set_shader_parameter("bitcodeMap", generateBitcodeMap())
+	shaderMaterial.set_shader_parameter("offSet", get_used_rect().position)
+	shaderMaterial.set_shader_parameter("tileSize", tile_set.tile_size)
